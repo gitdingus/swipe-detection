b37ca4b62f665d7a754a05e6504c44d52eca001d
Josh S <aaabccc87@protonmail.com>
2022-07-28 20:45:47 -0400

    Add library type

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
+    library: {
+      type: 'umd',
+    },
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
